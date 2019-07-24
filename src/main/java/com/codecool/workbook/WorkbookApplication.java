247b956f4cb8ec619cd4daee6ab14cb9aaf4d367
Petrav <vagi.p94@gmail.com>
2019-07-24 13:32:23 +0200

    First 5 questions

--- src/main/java/com/codecool/workbook/WorkbookApplication.java
+++ src/main/java/com/codecool/workbook/WorkbookApplication.java
@@ -10,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import javax.annotation.PostConstruct;
 import java.util.Arrays;
+import java.util.Collections;
 
 @SpringBootApplication
 public class WorkbookApplication {
@@ -23,7 +24,45 @@ public class WorkbookApplication {
 
     @PostConstruct
     public void afterInit() {
-        System.out.println(questionDaoMem);
+        //Until we have DB, Questions will be instantiated here
+
+        questionDaoMem.addQuestion(
+                new Question("What is casting? What is the difference between up vs downcasting?",
+                        "Casting means taking an Object of one type and turning it into a different " +
+                                "Object type. With downcasting, we are turning the Object into a subtype, " +
+                                "and with upcasting we are turning it into a supertype. Upcasting is always allowed, " +
+                                "but downcasting involves a type check that can throw a ClassCastException.",
+                        Collections.singletonList(Tag.PROGRAMMING_PARADIGMS), Room.OOP)
+        );
+        questionDaoMem.addQuestion(
+                new Question("What is a constructor?",
+                        "Constructors are special methods invoked when an object is created and are used to " +
+                                "initialize them. A constructor can be used to provide initial values " +
+                                "for object attributes.",
+                        Arrays.asList(Tag.OOP, Tag.JAVA), Room.OOP)
+        );
+        questionDaoMem.addQuestion(
+                new Question("What is method overloading?",
+                        "Method overloading allows us to have more than one method having the same name, " +
+                                "if the parameters of methods are different in number, sequence or data types. " +
+                                "At compile time, Java knows which method to invoke by checking the method signatures. " +
+                                "So this is called compile time polymorphism.",
+                        Arrays.asList(Tag.OOP, Tag.JAVA), Room.OOP)
+        );
+        questionDaoMem.addQuestion(
+                new Question("How many instances can be created for an abstract class?",
+                        "An abstract class cannot be instantiated.",
+                        Arrays.asList(Tag.OOP, Tag.JAVA), Room.OOP)
+        );
+        questionDaoMem.addQuestion(
+                new Question("What is the default access modifier in a class?",
+                        "When no access modifier is specified for a class - " +
+                                "It is said to be having the default access modifier by default - " +
+                                "it is accessible only within the same package.",
+                        Arrays.asList(Tag.OOP, Tag.JAVA), Room.OOP)
+        );
+
+        System.out.println(questionDaoMem.getRandomQuestion());
     }
 
 }
